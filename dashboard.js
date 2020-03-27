b5d7d46c7832e640e27d5472d74177a0d8ae3822
Willson Purl <purlwillson786@gmail.com>
2020-03-27 17:30:14 +0530

    done with all functionality

---
+++ dashboard.js
@@ -0,0 +1,217 @@
+
+// Date and Time
+
+let date = new Date().toDateString().split(' ');
+
+let dateSection = document.querySelector('.date h4');
+let timeSection = document.querySelector('.date h1');
+
+dateSection.innerHTML = `${date[2]} ${date[1]} ${date[3]} - ${date[0]}`;
+
+setInterval(() => {
+  let hours = new Date().getHours();
+  let minutes = new Date().getMinutes();
+
+  let isAm = hours < 12 ? 'AM' : 'PM';
+  hours = hours % 12;
+  hours = hours === 0 ? 12 : hours;
+  hours = (hours/10 < 1) ? '0' + hours : hours;
+  minutes = (minutes/10 < 1) ? '0' + minutes : minutes;
+
+  timeSection.innerHTML = `${hours}:${minutes} <small>${isAm}</small>`;
+}, 1000);
+
+// Background Image
+
+let images = document.querySelectorAll('.modal-body img');
+let main = document.querySelector('main');
+
+images.forEach(img => {
+  img.addEventListener('click', e => {
+    if(e.target.hasAttribute('src'))
+    {
+      let src = e.target.getAttribute('src');
+      main.style.background = `url(${src}) no-repeat center center`;
+      main.style.backgroundSize = 'cover';
+    }
+  })
+})
+
+//  Weather Info
+
+function weather(){
+
+  quoteFun();
+
+  let long;
+  let lat;
+  let tempSummary = document.querySelector('.deg');
+  let temp = document.querySelector('.temp');
+  let press = document.querySelector('.pressure');
+  let hum = document.querySelector('.humidity');
+  let wind = document.querySelector('.wind');
+  let wIcon = document.querySelector('.weather-icon');
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      let proxy = 'https://cors-anywhere.herokuapp.com/';
+      var api = `${proxy}https://api.darksky.net/forecast/fdf36c1e47d51baa0462804ed7bc27b1/${lat},${long}`;
+
+   fetch(api)
+    .then(res => {return res.json()})
+    .then(data => {
+      console.log(data);
+      let { temperature, summary, humidity, pressure, windSpeed, icon } = data.currently; 
+
+      //      Set dom elements
+
+      let celsius = Math.floor((temperature - 32) * 5/9);
+      let humPrs = Math.floor(humidity * 100);
+
+      tempSummary.textContent = `${celsius}° ${summary}`;
+      temp.textContent = `Temperature : ${celsius}° C`;
+      press.textContent = `Pressure : ${pressure} hPa `;
+      hum.textContent = `Humidity : ${humPrs}%`;
+      wind.textContent = `Wind : ${windSpeed} km/h`;
+
+      if(icon === 'rain'){
+        icon = 'cloud-showers-heavy';
+      } else if(icon === 'partly-cloudy-day'){
+        icon = 'cloud-sun';
+      } else if(icon === 'partly-cloudy-night'){
+        icon = 'cloud-moon';
+      } else if(icon === 'snow'){
+        icon = 'cloud-rain';
+      } else {
+        icon = 'cloud';
+      }
+
+      wIcon.innerHTML = `<i class="fas fa-${icon} my-2"></i>`
+
+    })
+    .catch(err => console.log(err));
+      
+    });
+    
+  }
+}
+
+window.onload = weather;
+
+//      Fetch Quote
+
+let quoteElem = document.querySelector('.quote');
+
+const quoteFun = async function(){
+
+const api = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+let res = await fetch(api);
+let data = await res.json();
+let quotes = await data.quotes;
+let randomNum = Math.floor(Math.random() * 90 + 1);
+
+let quote = quotes[randomNum].quote;
+console.log(quote);
+quoteElem.innerHTML = `<sup><i class="fas fa-quote-left"></i></sup> ${quote}`;
+
+}
+
+//    Stop Watch
+
+let seconds = '00';
+let minutes = '00';
+let hours = '00';
+let status = "stopped";
+let startInterval = null;
+
+let displayTiming = document.querySelector('.timing');
+const WatchBtn = document.querySelector('#play-pause-watch');
+
+WatchBtn.addEventListener('click', stopWatch);
+
+function startWatch(e){
+    +seconds++;
+    seconds = addZero(seconds);
+
+    if(+seconds / 60 === 1){  
+        seconds = '00';
+        +minutes++;
+        minutes = addZero(minutes);
+
+
+        if(+minutes / 60 === 1){
+            minutes = '00';
+            +hours++;
+            hours = addZero(hours);
+        }
+    }
+
+
+    displayTiming.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+}
+
+function addZero(n){
+  if(n < 10){
+    return '0' + n;
+  } 
+  return n;
+}
+
+function stopWatch(e){
+  if(status === "stopped"){
+
+    interval = setInterval(startWatch, 1000);
+    let elem = document.getElementById("play-pause-watch");
+    elem.className = 'far fa-pause-circle';
+    status = "started";
+
+  }
+  else{
+      clearInterval(interval);
+      let elem = document.getElementById("play-pause-watch");
+      elem.className = 'far fa-play-circle';
+      status = "stopped";
+
+  }
+}
+
+let resetWatch = document.getElementById('reset-watch');
+resetWatch.addEventListener('click', resetStopWatch);
+
+function resetStopWatch(){
+    clearInterval(interval);
+    seconds = '00';
+    minutes = '00';
+    hours = '00';
+    document.querySelector(".timing").innerHTML = "00:00:00";
+    document.querySelector("#play-pause-watch").className = 'far fa-play-circle';
+}
+
+//        Music
+
+let player = document.getElementById('music-player');
+let description = document.getElementById('music-des');
+
+let studyBtn = document.getElementById('study-btn');
+let meditationBtn = document.getElementById('meditation-btn');
+let codingBtn = document.getElementById('coding-btn');
+
+studyBtn.addEventListener('click', function(){
+  player.src = './music/study.mp3';
+  description.innerHTML = 'Study Music';
+})
+
+meditationBtn.addEventListener('click', function(){
+  player.src = './music/meditation.mp3';
+  description.innerHTML = 'Meditation Music';
+})
+
+codingBtn.addEventListener('click', function(){
+  player.src = './music/coding.mp3';
+  description.innerHTML = 'Coding Music';
+})
---
+++ todos.js
@@ -0,0 +1,147 @@
+//        Todo List
+
+let todoInput = document.getElementById('todo');
+let todoBtn = document.getElementById('add-todo-btn');
+let todoSection = document.getElementById('custom-todos');
+
+//      Init fun
+
+function init(){
+  let initTodos = JSON.parse(localStorage.getItem('todos'));
+
+  if(initTodos){
+    makeTodo();
+  }
+}
+
+init();
+
+
+todoBtn.addEventListener('click', addTodo);
+
+function addTodo(){
+  let todo = todoInput.value;
+  let todos = [];
+
+  if(todo === undefined || todo === ''){
+    return;
+  }
+
+  if(localStorage.getItem('todos') === null)
+  {
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  } 
+
+  makeTodo();
+
+  todoInput.value = '';
+}
+
+function makeTodo(){
+  todoSection.innerHTML = '';
+  let completeTodos = JSON.parse(localStorage.getItem('completeTodos')) ? JSON.parse(localStorage.getItem('completeTodos')) : [];
+
+  let allTodos = JSON.parse(localStorage.getItem('todos'));
+
+  allTodos.forEach(todo => {
+
+  let todoRow = document.createElement('div');
+  let todoCol_1 = document.createElement('div');
+  let todoCol_2 = document.createElement('div');
+
+  todoRow.className = 'todo row shadow p-2 no-gutters my-2';
+  todoCol_1.className = 'col-10 d-flex justify-content-start align-items-center';
+  todoCol_2.className = 'col-2 align-self-center';
+
+  let checkBtn = document.createElement('i');
+  let deleteBtn = document.createElement('i');
+
+  checkBtn.className = 'far fa-circle mx-4';
+  deleteBtn.className = 'far fa-trash-alt fa-2x';
+
+  if(completeTodos){
+    completeTodos.forEach(t => {
+      if(t === todo){
+        checkBtn.className = 'far fa-check-circle mx-4';
+        todoRow.style.backgroundColor = '#00C9A7';
+      }
+    })
+  }
+
+  let todoText = document.createElement('h5');
+  todoText.className = 'align-self-end';
+  todoText.innerHTML = `${todo}`;
+
+  todoCol_1.append(checkBtn);
+  todoCol_1.append(todoText);
+  todoCol_2.append(deleteBtn);
+
+  todoRow.append(todoCol_1);
+  todoRow.append(todoCol_2);
+
+  todoSection.append(todoRow);
+
+  //      Complete Todo
+
+  checkBtn.addEventListener('click', e => {
+    let c = e.target.className;
+    
+    if(c === 'far fa-circle mx-4'){ 
+      if(localStorage.getItem('completeTodos') === null)
+        {
+          completeTodos.push(todo);
+          localStorage.setItem('completeTodos', JSON.stringify(completeTodos));
+
+        } else {
+          completeTodos = JSON.parse(localStorage.getItem('completeTodos'));
+          completeTodos.push(todo);
+          localStorage.setItem('completeTodos', JSON.stringify(completeTodos));
+        } 
+
+      e.target.className = 'far fa-check-circle mx-4';
+      todoRow.style.backgroundColor = '#00C9A7';
+    } 
+    else {
+      completeTodos = completeTodos.filter(t => {
+        if(t !== todo){
+          return t;
+        }
+      });
+
+      localStorage.setItem('completeTodos', JSON.stringify(completeTodos));
+
+      e.target.className = 'far fa-circle mx-4';
+      todoRow.style.backgroundColor = '#FF9671';
+    }
+  })
+
+  //      Delete Todo
+
+    deleteBtn.addEventListener('click', e => {
+      let remainTodos = allTodos.filter(t => {
+        if(t !== todo){
+          return t;
+        }
+      });
+
+      let remainCpltTodos = completeTodos.filter(t => {
+        if(t !== todo){
+          return t;
+        }
+      })
+
+    localStorage.setItem('todos', JSON.stringify(remainTodos));
+    localStorage.setItem('completeTodos', JSON.stringify(remainCpltTodos));
+    makeTodo();
+
+    });
+
+  });
+
+}
